4202f9c87140c6c7b37ea5cb97f9c918aa1ca3ff
Riley <rileyw13@protonmail.com>
2020-05-16 12:55:06 -0400

    initial commit

---
+++ data/README.md
@@ -0,0 +1,11 @@
+# Test Information
+These tests were run to compare how my speeds changed as I incrementally
+upgraded the equipment in my desktop. Below are descriptions of the equipment
+changes.
+
+## 00-no-upgrades.csv
+This was the initial test, the control. My computer was plugged into an old
+router that we were using as a switch, and the router was plugged into a wall
+jack that led back to the main router. The router we were using as a switch was
+one of those "family" routers that filter content and I was locked out of the
+admin panel so I couldn't properly configure it.
---
+++ repeated-speedtest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# uses cli for speedtest.net, available here:
+# https://www.speedtest.net/apps/cli
+
+# to see list of servers, run "speedtest -L". These are at or close to
+# Charlotte, NC
+SERVERS="4211 5028 12784"
+NUM_TRIALS=10
+
+# first test will use automatically chosen "best server"
+speedtest --output-header -f csv
+
+# repeated trials
+for i in $(seq $NUM_TRIALS); do
+  for server in $SERVERS; do
+    # sleep a random, short amount of time
+    sleep $((3 + RANDOM % 17))
+    # run the test
+    speedtest -s $server -f csv
+
+    # debug
+    # echo "trial number $i"
+    # echo "speedtest -s $server -f csv"
+  done
+done
