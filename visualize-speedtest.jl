a77fc0ee8c6100d929c2f063261880cccbd964ea
Riley <rileyw13@protonmail.com>
2020-05-16 15:07:54 -0400

    wrote function to make plots

--- visualize-speedtest.jl
+++ visualize-speedtest.jl
@@ -25,11 +25,84 @@ function load_data()
   data = []
   for file in file_names
     println("loading data from ", file)
-    append!(data, [CSV.File(file)])
+    append!(data, [CSV.File(file; normalizenames=true)])
   end
+  println("finished!")
+  println()
 
   file_names, data
 end
 
-function plot_data()
+bytes_to_mbits(x) = x * 1e-6 * 8
+
+function convert_speedtest_up_down_to_mbits_per_s!(data)
+  metrics = [:download, :upload]
+
+  for i = 1:size(data)[1]
+    for metric in metrics
+      metric_data = getproperty(data[i], metric)
+      println(metric_data)
+      converted_data = bytes_to_mbits.(metric_data)
+      println(converted_data)
+      println(data[i])
+
+      # doesn't work
+      # setproperty!(data[i], metric, converted_data)
+    end
+  end
+
+  data
+end
+
+function plot_data(file_names, 
+    data, 
+    metrics=[:latency, :jitter, :download, :upload]
+  )
+
+  x = []
+  for i = 1:size(file_names)[1]
+    m = match(r".*\/(.*)\.csv", file_names[i])
+    for metric in metrics
+      for entry in getproperty(data[i], metric)
+        append!(x, [string(m[1], "_", metric)])
+      end
+    end
+  end
+
+  y = []
+  for single_test_data in data
+    for metric in metrics
+      if metric == :download || metric == :upload
+        append!(y, bytes_to_mbits.(getproperty(single_test_data, metric)))
+      else
+        append!(y, getproperty(single_test_data, metric))
+      end
+    end
+  end
+
+  # set backend
+  gr()
+
+  println(x)
+  println(y)
+
+  # create plot
+  plot(x, y,
+    seriestype = :scatter, 
+    title = "Internet speeds after different upgrades",
+    xlabel = "Upgrade and metric",
+    ylabel = "Value",
+    dpi = 700,
+    ylims = (0.0, 100),
+    legend = false,
+    # markersize = 1,
+    # markeralpha = 1,
+    # markeralpha = 0.1,
+    # markerstrokestyle = :dot,
+    # markerstrokecolor = :blue,
+    # markerstrokewidth = 0,
+    markerfillcolor = :blue,
+  )
+  
+  savefig("plots/result.png")
 end
